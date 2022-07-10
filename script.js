68c65a9069b305c1e283dd31f3af3841f3cfb755
Shelery <eszter.with.a.z@gmail.com>
2022-07-10 14:37:00 +0200

    Add rows and grids of a predifined number

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 //      to dinamically create 25x25 grid
 const canvas = document.querySelector('#canvas')
 for (let i = 0; i < 25; i++){
-const grid = document.createElement('div')
-grid.classList.add('grid')
-canvas.appendChild(grid)
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (let j = 0; j < 25; j++){
+        const grid = document.createElement('div')
+        grid.classList.add('grid')
+        row.appendChild(grid)
+    }
+    canvas.appendChild(row)
+
 }
 // Connect it to the input of #range
 //      NOTE its return value is string
