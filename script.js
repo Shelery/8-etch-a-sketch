9cecc26cbde47bc0d8c4a1d47aeebb2d0155cae0
Shelery <eszter.with.a.z@gmail.com>
2022-07-11 12:33:48 +0200

    Add paintGrid function
    paintGrid function is called when clicked on a grid.
    It paints every grid it hovers above.
    NOTE Currently it only works on initial (25*25) canvas

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-const canvas = document.querySelector('#canvas');
+window.addEventListener('DOMContentLoaded', (event) => {
+    
+
+//const canvas = document.querySelector('#canvas');
 // Connect it to the input of #range
 //      NOTE its return value is string
 //      remove grids of previous input
 //     LATER:
 //      remove colored grids of previous input
-const range = document.querySelector('#range');
-let value = range.addEventListener('click', ()=>{
-    // remove grid of previous input
-    removeGrids();
-    value = range.value;
-    console.log(value);
-    //return value;
+    const range = document.querySelector('#range');
+    let value = range.value;
+
     createGrids(value);
+    let grids = document.querySelectorAll('.grid')
+    grids.forEach(grid => grid.addEventListener('mousedown', paintGrid));
 
-})
-// JS function 
-//      to dinamically create 25x25 grid
+    range.addEventListener('click', ()=>{
+        // remove grid of previous input
+        removeGrids();
+        value = range.value;
+        //return value;
+        createGrids(value);
+    })
 
 
+});     
 
-
-// Make grids colored when mouse down
+// [] Make grids colored when mouse down
 //      using classList.switch(?)
-// Randomise color - IDEA pride month edition - colors always change
+
+// []Randomise color - IDEA pride month edition - colors always change
 
 // FUNCTIONS
-function removeGrids(){
-    while (canvas.firstChild){
-        canvas.removeChild(canvas.firstChild);
-     }
-}
+// JS function 
+//      to dinamically create 25x25 grid
 function createGrids(value){
     for (let i = 0; i < value; i++){
         const row = document.createElement('div');
@@ -40,5 +43,26 @@ function createGrids(value){
             row.appendChild(grid);
         }
         canvas.appendChild(row);
+        
 }
+    
 }
+function removeGrids(){
+    while (canvas.firstChild){
+        canvas.removeChild(canvas.firstChild);
+     }
+}
+
+function paintGrid(e){
+    e.target.setAttribute('style', 'background-color: black;')
+    let grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => grid.addEventListener('mouseover', ()=>{
+        grid.setAttribute('style', 'background-color: black;')
+     }));
+    //grids.forEach(grid => grid.addEventListener('mouseover', paint()));
+    // grids.forEach(grid => grid.addEventListener('mouseup', (e)=> {
+    //     grid.removeEventListener('mouseover')
+    // }));
+}
+// DOESNT WORK ON DIFFERENT GRIDS
+// not that it worked anyways... i couldnt remove anonym function
